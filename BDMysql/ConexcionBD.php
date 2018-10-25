c13f05beb0a2b1c67c3c865d2a5abe2526a502e9
RafaMateos <rafaelmateostorres@gmail.com>
2018-10-25 11:02:33 +0200

    Hacer

---
+++ BDMysql/ConexcionBD.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rmateos
+ * Date: 22/10/18
+ * Time: 11:13
+ */
+
+$conexion = new mysqli('localhost','root','root','Personas');
---
+++ BDMysql/Personas.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: rmateos
+ * Date: 22/10/18
+ * Time: 10:52
+ */
+class Personas
+{
+
+    private $Nombre;
+    private $Edad;
+    private $Direccion;
+    private $Telefono;
+
+    public function __construct($newNombre,$newEdad,$newDireccion,$newTelefono)
+
+    {
+
+         $this->Nombre = $newNombre;
+         $this->Edad = $newEdad;
+         $this->Direccion = $newDireccion;
+         $this->Telefono = $newTelefono;
+
+    }
+
+   public function getNombre(){
+
+        return $this->Nombre;
+
+   }
+
+   public function getEdad(){
+
+       return $this->Edad;
+
+   }
+
+   public function getDireccion(){
+
+       return $this->Direccion;
+
+   }
+
+   public function getTelefono(){
+
+       return $this->Telefono;
+   }
+
+
+
+}
